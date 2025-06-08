4c90f2d415768da76800997e83269bd3dbd0b6e7
Harrison Clark <harrisonclark0915@gmail.com>
2025-06-08 17:01:41 -0500

    Removed some generic placeholders, added some social links, added a test blog post.

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'ClarkSec';
+export const SITE_DESCRIPTION = 'Cybersecurity blog, writeups, and insights by Harrison Clark.';
---
+++ src/content/blog/test.md
@@ -0,0 +1,8 @@
+---
+title: 'Test Post'
+description: 'This is a simple test post.'
+pubDate: 'Jun 08 2025'
+heroImage: '/images/test.jpg' 
+---
+
+This is a test blog post to verify everything is working correctly with the date format you want.
